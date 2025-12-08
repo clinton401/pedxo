2fd650dd2b0cfe6272ec4c54c96ddf53cbafc996
clinton <clintonphillips464@gmail.com>
2025-12-08 22:14:04 +0100

    fixed spinner animation time

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -39,7 +39,7 @@ const Socials = ({ isRegisterPage = false }) => {
         {loadingProvider === "github" ? (
           <span className="">
             <svg
-              className="animate-spin h-5 w-5 text-gray-600"
+              className="animate-spin  [animation-duration:0.5s] h-5 w-5 text-gray-600"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
@@ -77,7 +77,7 @@ const Socials = ({ isRegisterPage = false }) => {
         {loadingProvider === "google" ? (
           <span className="">
             <svg
-              className="animate-spin h-5 w-5 text-gray-600"
+              className="animate-spin  [animation-duration:0.5s] h-5 w-5 text-gray-600"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
